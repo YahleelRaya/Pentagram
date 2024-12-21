b7130627942ab09bb0d396fb2f13bf35207fd06d
YahleelRaya <yahleelxo100@gmail.com>
2024-12-21 12:55:36 -0500

    added server actions in comp and action file

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -7,11 +7,28 @@ export async function POST(request: Request) {
 
     // TODO: Call your Image Generation API here
     // For now, we'll just echo back the text
+    const modalApiUrl = `https://yahleelraya--pentagram-inference-generate-dev.modal.run?prompt=${encodeURIComponent(text)}`;
 
-    return NextResponse.json({
-      success: true,
-      message: `Received: ${text}`,
+    const response = await fetch(modalApiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`  
+      },
     });
+
+    if (!response.ok) {
+      throw new Error(`Failed to generate image: ${response.statusText}`);
+    }
+    const imageBlob = await response.blob();
+
+    return new Response(imageBlob, {
+      status: 200,
+      headers: {
+        'Content-Type': 'image/png',
+      },
+    });
+
   } catch (error) {
     return NextResponse.json(
       { success: false, error: "Failed to process request" },
